9f50989f67213a6cdc28d6c90e02b4c1c26806ba
Sorarinu <sorarinu0814@gmail.com>
2022-03-21 15:44:06 +0900

    feat: setup logger

--- app/cmd/main.go
+++ app/cmd/main.go
@@ -2,16 +2,84 @@ package main
 
 import (
 	"net/http"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
+
+var logLevelSeverity = map[zapcore.Level]string{
+	zapcore.DebugLevel:  "DEBUG",
+	zapcore.InfoLevel:   "INFO",
+	zapcore.WarnLevel:   "WARNING",
+	zapcore.ErrorLevel:  "ERROR",
+	zapcore.DPanicLevel: "CRITICAL",
+	zapcore.PanicLevel:  "ALERT",
+	zapcore.FatalLevel:  "EMERGENCY",
+}
+
+func EncodeLevel(l zapcore.Level, enc zapcore.PrimitiveArrayEncoder) {
+	enc.AppendString(logLevelSeverity[l])
+}
+
+func newEncoderConfig() zapcore.EncoderConfig {
+	cfg := zap.NewProductionEncoderConfig()
+	cfg.TimeKey = "timestamp"
+	cfg.LevelKey = "severity"
+	cfg.MessageKey = "message"
+	cfg.EncodeLevel = EncodeLevel
+	cfg.EncodeTime = zapcore.RFC3339NanoTimeEncoder
+
+	return cfg
+}
+
+func NewCloudLoggingLogger() *zap.Logger {
+	cfg := zap.NewProductionConfig()
+	cfg.InitialFields = map[string]interface{}{"appVersion": "v1.0.0"}
+	cfg.EncoderConfig = newEncoderConfig()
+	logger, _ := cfg.Build()
+	return logger
+}
 
 func main() {
 	router := gin.Default()
+
+	// CORS 許可
+	config := cors.DefaultConfig()
+	config.AllowOrigins = []string{
+		"*",
+	}
+	config.AllowMethods = []string{
+		"POST",
+		"PUT",
+		"DELETE",
+		"GET",
+		"OPTIONS",
+	}
+	config.AllowHeaders = []string{
+		"Content-Type",
+		"Content-Length",
+		"Authorization",
+		"X-CSRF-Token",
+		"Accept-Encoding",
+		"Access-Control-Allow-Headers",
+	}
+	config.MaxAge = 24 * time.Hour
+	router.Use(cors.New(config))
+
+	logger := NewCloudLoggingLogger()
+	defer logger.Sync()
+	zap.ReplaceGlobals(logger)
+
+	// zap.L().Info("Info Message")
+
 	router.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "Hello World!!",
 		})
 	})
-	router.Run()
+
+	router.Run(":8080")
 }
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.17
 require github.com/gin-gonic/gin v1.7.7
 
 require (
+	github.com/gin-contrib/cors v1.3.1 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
@@ -16,6 +17,9 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/ugorji/go/codec v1.2.7 // indirect
+	go.uber.org/atomic v1.9.0 // indirect
+	go.uber.org/multierr v1.8.0 // indirect
+	go.uber.org/zap v1.21.0 // indirect
 	golang.org/x/crypto v0.0.0-20220315160706-3147a52a75dd // indirect
 	golang.org/x/sys v0.0.0-20220319134239-a9b59b0215f8 // indirect
 	golang.org/x/text v0.3.7 // indirect
